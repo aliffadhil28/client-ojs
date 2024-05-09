1e4f0cb92aa5175d49b285cc76ab7d579a9320c5
aliffadhil28 <alif70662@gmail.com>
2024-05-09 19:29:00 +0700

    fix : vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { root } from 'postcss'
 
-const outDir = resolve(__dirname,'dist')
+const outDir = resolve('/dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
