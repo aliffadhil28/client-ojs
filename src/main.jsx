49d71084a2dacf2572b3473da8429df507c64832
aliffadhil28 <alif70662@gmail.com>
2024-05-09 17:09:56 +0700

    fix: main js

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { navbarTheme } from "./assets/themes/navbarTheme.js";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Code from "./pages/code/Code.jsx";
 import Home from "./pages/home/Home.jsx";
-import Login from "./pages/Login.jsx";
+import Login from "./pages/login/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Problem from "./pages/admin/problems/problem/Problems.jsx";
 import ProblemDetail from "./pages/admin/problems/detail/ProblemDetails.jsx";
@@ -17,7 +17,7 @@ import { AuthContextProvider } from "./assets/context/AuthContext.jsx";
 import HomeLayout from "./assets/layouts/HomeLayout.jsx";
 import DashboardLayout from "./assets/layouts/DashboardLayout.jsx";
 import Solutions from "./pages/admin/solutions/Solutions.jsx";
-import Profile from "./pages/Profile.jsx";
+import Profile from "./pages/profile/Profile.jsx";
 import Users from "./pages/admin/users/Users.jsx";
 import App from "./App.jsx";
 
